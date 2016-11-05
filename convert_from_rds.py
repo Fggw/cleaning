9c6d19bffdba5edd5eadcdb285caef41ba4bf619
Samuel Green <green.sam1192@gmail.com>
2016-11-05 12:19:18 -0400

    convert rds file to csv

---
+++ convert_from_rds.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""
+Script to convert r data files into .csv
+"""
+
+import os
+import sys
+import rpy2.robjects as r
+from rpy2.robjects import pandas2ri
+
+pandas2ri.activate()
+
+# find the relevant files
+BASE = '../chicago/DATA/'
+FILES = [f for f in os.listdir(BASE) if '.Rds' in f]
+
+print FILES
+
+for f in FILES:
+
+    name, ext = f.split('.')
+    
+    try:
+        obj = r.r['readRDS'](BASE + f)
+        pyobj = pandas2ri.ri2py(obj)
+        outname = "{}.csv".format(name)
+
+        if not os.path.isfile(outname):
+            print "Writing", outname
+            pyobj.to_csv(outname)
+
+    except Exception as error:
+        print f, error    
+
+print "All done"
